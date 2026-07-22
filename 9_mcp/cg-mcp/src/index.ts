c4529e6b5b6484844663c1cfecec3d9a74b69297
krishmungase <mungasekrishna8@gmail.com>
2026-07-22 09:21:53 +0530

    feat: register refund-policy resource to MCP server

--- 9_mcp/cg-mcp/src/index.ts
+++ 9_mcp/cg-mcp/src/index.ts
@@ -11,6 +11,64 @@ const server = new McpServer({
     version: "1.0.0",
 });
 
+server.registerResource(
+    'refund-policy',
+    'https://codersgyan.com/refund-policy',
+    {
+        title: "Codersgyan Refund Policies",
+        mimeType: 'text/plainn',
+        description: 'Refund policy of the website'
+    },
+    async uri => ({
+        contents: [{
+            uri: uri.href,
+            mimeType: 'text/plain',
+            text:
+                `
+            At Coder's Gyan, we are committed to providing high-quality, valuable programming courses and materials to our learners. To ensure your complete satisfaction and confidence in your purchase, we now offer a 100% risk-free, 23-day money-back guarantee. Your satisfaction is our priority.
+
+            1. 23 Day Money-Back Guarantee
+            We believe in the value of our courses, and we want you to feel confident in your decision. If you’re not satisfied with your purchase for any reason, you can request a full refund within 23 days of your purchase — no risk, no questions asked.
+
+            2. How to Request a Refund
+            To initiate a refund request within the 23-day window, simply contact us at hello@codersgyan.com with your order details, and our team will process your refund promptly.
+
+            3. Important Notes:
+            Some importent points related to our Refund Policy:
+
+            ✔󠀿 Request Period: Refunds are only available within 23 days from the original purchase date.
+
+            ✔󠀿 No Late Refunds: Refund requests will not be accepted after 23 days.
+
+            ✔󠀿 One-Time Refund: Each course is eligible for a one-time refund per user only to prevent misuse.
+
+            ✔󠀿 Content Usage Limit: Refunds are not applicable if a significant portion of the course has already been consumed. To maintain fairness, users who have watched more than 3 hours of content or have downloaded a notable amount of course material will not be eligible for a refund during the 23-day window.
+
+            4. Commitment to Quality
+            We want you to be confident in what you’re buying. That’s why we offer:
+
+            ✔󠀿 Detailed Course Descriptions: Each course page outlines what you’ll learn, what’s included, and the skills you'll gain by the end of the course.
+
+            ✔󠀿 Module Breakdown: We show the structure of our courses, including the lessons and topics covered in each section, so you know exactly what to expect.
+
+            ✔󠀿 Preview Content: Access sample videos or introductory lessons for free, so you can evaluate our teaching style before making a purchase.
+
+            ✔󠀿 Free Modules (when available): Some courses include complete modules available at no cost, giving you hands-on exposure to the learning experience.
+
+            5. Contact us
+            If you have any questions regarding our courses or need further information before purchasing, please feel free to contact us at hello@codersgyan.com
+            `
+        }]
+    })
+);
+
+// server.registerResource(
+//     'greeting',
+//     new ResourceTemplate('greeting://{name}', { list: undefined }),
+//     { description: 'A greeting for the named subject' },
+//     async (uri, vars) => ({ contents: [{ uri: uri.href, text: `Hello, ${vars.name}!` }] })
+// );
+
 server.registerPrompt(
     "greeting-example",
     {
